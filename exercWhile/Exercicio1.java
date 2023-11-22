6e67a0b24cea6c27f5977a3b136095e592677cd8
Andrzinh <146098592+Andrzinh@users.noreply.github.com>
2023-11-22 17:36:03 -0300

    Update Exercicio1.java

--- exercWhile/Exercicio1.java
+++ exercWhile/Exercicio1.java
@@ -2,10 +2,10 @@ public class Exercicio1 {
 	public static void main(String[] args) {
 		double i=0, j=134, p=145;
 		while(p>=j) {
-			j = j + 2.5;
-			p = p + 2;
+			j=j+2.5;
+			p=p+2;
 			i++;
 		}
-		 System.out.println("jo„o demora "+ i +" anos para ficar maior que pedro");
+		 System.out.println("Jo√£o demora "+ i +" anos para ficar maior que pedro");
 	}
 }
