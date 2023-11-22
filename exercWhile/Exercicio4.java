30d470006c29a7e91363815985bb849aa2a6b4f7
Andrzinh <146098592+Andrzinh@users.noreply.github.com>
2023-11-22 17:33:37 -0300

    Update Exercicio4.java

--- exercWhile/Exercicio4.java
+++ exercWhile/Exercicio4.java
@@ -3,28 +3,23 @@ public class Exercicio4 {
 
 	public static void main(String[] args) {
 		Scanner ler = new Scanner(System.in);
-		int a, b, c;
+		int a=0, b=1, c, n, i=2;
 		a = 0;
 		b = 1;
 		
 		System.out.print("Digite um numero:  ");
-		int n = ler.nextInt();
-		int i = 2;
-		
-		if (n >= 1) {
-			System.out.print(b+"  ");
+		n = ler.nextInt();
+		if (n>=1){
+			System.out.print(b+"");
 		}
-		
-		while (i < n) {
+		while (i<n){
 			c = a + b;
-		    a = b;
-		    b = c;
+			a = b;
+			b = c;
 			
-			i = i + 1;
-			System.out.print(c+"  ");
+			i=i+1;
+			System.out.print(c+"");
 		}
-		ler.close();
 	}
-
 }
 
