84adaba47967ce3dcec846534ee734eeaa45718c
Andrzinh <146098592+Andrzinh@users.noreply.github.com>
2023-11-22 21:44:09 -0300

    Update and rename V_diferentes.java to exercicio2.java

--- exercDesafioVetor/exercicio2.java
+++ exercDesafioVetor/exercicio2.java
@@ -1,41 +1,38 @@
-package exs15;
-
 import java.util.*;
-
-public class V_diferentes {
+public class exercicio2 {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		
-		final int Tam = 10;
-		int[] A = new int[Tam];
-		int[] B = new int[Tam];
-		int[] C = new int[Tam];
+		final int TAM = 10;
+		int[] A = new int[TAM];
+		int[] B = new int[TAM];
+		int[] C = new int[TAM];
 		int k = 0;
 		
 		System.out.println("escreva 10 numeros para o conjunto A: ");
-		for (int i = 0; i < Tam; i++) {
+		for (int i = 0; i < TAM; i++) {
 			A[i] = in.nextInt();
 		}
 		System.out.println("escreva 10 numeros para o conjunto B: ");
-		for (int i = 0; i < Tam; i++) {
+		for (int i = 0; i < TAM; i++) {
 			B[i] = in.nextInt();
 			in.close();
 		}
 
-		for (int i = 0; i < Tam; i++) {
+		for (int i = 0; i < TAM; i++) {
 			int j;
-			for (j = 0; j < Tam; j++) {
+			for (j = 0; j < TAM; j++) {
 				if (A[i] == B[j]) {
 					break;
 				}
 			}
-			if (j == Tam) {
+			if (j == TAM) {
 				C[k] = A[i];
 				k++;
 			}
 		}
 
-		System.out.println("Vetor C (diferença dos conjuntos A e B): ");
+		System.out.println("Vetor C (diferenÃ§a dos conjuntos A e B): ");
 		for (int i = 0; i < k; i++) {
 			System.out.print(C[i] + " ");
 		}
