cfe9fcd8879cac0601ac8de96d6c27b4450c5758
Andrzinh <146098592+Andrzinh@users.noreply.github.com>
2023-11-22 21:57:11 -0300

    Update and rename V_parImpar.java to exercicio8.java

--- exercDesafioVetor/exercicio8.java
+++ exercDesafioVetor/exercicio8.java
@@ -1,22 +1,16 @@
-package exs15;
-
-import java.util.*;
-
-public class V_parImpar {
+import java.util.Scanner;
+public class exercicio8 {
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
+		Scanner ler = new Scanner(System.in);
 
-		// declarando variáveis
 		final int TAM = 20;
 		int i, a[], b[];
 		a = new int[TAM];
 		b = new int[TAM];
 
-		// criando laços
 		for (i = 0; i < TAM; i++) {
-			System.out.print("Entre com o " + (i + 1) + "o. número:");
+			System.out.print("Entre com o " + (i + 1) + "o. nÃºmero:");
 			a[i] = in.nextInt();
-			// indentificando pares e impares
 			if (a[i] % 2 == 0) {
 				b[i] = a[i];
 				System.out.println("Par.");
