0c2ef4fac06d66898ac5d0182eb2f048ca3a9e08
Andrzinh <146098592+Andrzinh@users.noreply.github.com>
2023-11-22 21:52:17 -0300

    Update V_inteirosPares.java

--- exercDesafioVetor/V_inteirosPares.java
+++ exercDesafioVetor/V_inteirosPares.java
@@ -1,23 +1,18 @@
-package exs15;
-import java.util.*;
+import java.util.Scanner;
 public class V_inteirosPares {
 	public static void main(String[] args) {
-		
-		Scanner in = new Scanner(System.in);
+		Scanner ler = new Scanner(System.in);
 
-		// declarando variáveis
 		final int TAM = 10;
 		int i, a[], n;
 		a = new int[TAM];
-
-		// criando laços
 		for (i = 0; i < TAM; i++) {
-			System.out.print("Entre com o " + (i + 1) + "°. número:");
+			System.out.print("Entre com o " + (i + 1) + "Â°. nÃºmero:");
 			a[i] = in.nextInt();
 		}
 		for (i = 0; i < TAM; i++) {
 			System.out.println("Elemento " + (i + 1) + " do vetor A: " + a[i]);
-			System.out.println(" Pares de 0 até o elemento " + a[i] + ": ");
+			System.out.println(" Pares de 0 atÃ© o elemento " + a[i] + ": ");
 
 			for (n = 0; n <= a[i]; n++) {
 				if (n % 2 == 0) {
