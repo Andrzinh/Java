39fc3ea5b50c50ebd1eed1e659f9e842cdd658f9
Andrzinh <146098592+Andrzinh@users.noreply.github.com>
2023-11-22 22:03:50 -0300

    Update and rename V_parImparComVetorB.java to exercicio9.java

--- exercDesafioVetor/exercicio9.java
+++ exercDesafioVetor/exercicio9.java
@@ -1,20 +1,15 @@
-package exs15;
-
-public class V_parImparComVetorB {
+public class exercicio9 {
 	public static void main(String[] args) {
 
-		// declarando variáveis
 		final int TAM = 10;
 		int i, a[], b[];
 		a = new int[TAM];
 		b = new int[TAM];
 
-		// criando laços
 		for (i = 0; i < TAM; i++) {
 			a[i] = i + 1;
 			System.out.println("Vetor A[ " + a[i] + " ].");
 
-			// atribuindo condições
 			if (a[i] % 2 == 0) {
 				b[i] = 1;
 				System.out.println("Par. Vetor B[ " + b[i] + " ].");
